b4f2de45278d01c84ab8d8117cddbc8e9214f4f1
Amol Patil <amol17092000@gmail.com>
2024-07-11 23:26:18 +0530

    initial commit

---
+++ shareRide/src/main/java/com/shareRide/shareRide/ShareRideApplication.java
@@ -0,0 +1,13 @@
+package com.shareRide.shareRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareRideApplication.class, args);
+	}
+
+}
---
+++ shareRide/src/test/java/com/shareRide/shareRide/ShareRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareRide.shareRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
